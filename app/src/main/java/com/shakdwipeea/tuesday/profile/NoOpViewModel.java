fea78401310490920134f36188d7eaaa631ebe28
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2017-01-26 21:26:10 +0530

    add edit detail type p1

---
+++ app/src/main/java/com/shakdwipeea/tuesday/profile/NoOpViewModel.java
@@ -0,0 +1,8 @@
+package com.shakdwipeea.tuesday.profile;
+
+/**
+ * Created by akash on 24/1/17.
+ */
+
+public class NoOpViewModel {
+}
---
+++ app/src/main/java/com/shakdwipeea/tuesday/profile/edit/EditProfileItemViewModel.java
@@ -0,0 +1,8 @@
+package com.shakdwipeea.tuesday.profile.edit;
+
+/**
+ * Created by akash on 24/1/17.
+ */
+
+public class EditProfileItemViewModel {
+}
